229af0aaf1c10d9d61cd8b54cc736f5c4c22f1e0
Mr. Zero <vivek248.vm@gmail.com>
2024-06-24 00:08:40 +0530

    section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Grid } from '@mui/material';
 import Projects from "./Projects";
 import About from "./About";
 import Contact from "./Contact";
@@ -29,18 +30,26 @@ const Home = () => {
   }, [location]);
 
   return(
-    <>
-      <section id="home" ref={homeRef} style={{height: 500}}><h1>This is home page</h1></section>
-      <section ref={aboutRef} id="about" style={{ padding: '50px 0' }}>
-        <About />
-      </section>
-      <section ref={projectsRef} id="projects" style={{ padding: '50px 0' }}>
-        <Projects />
-      </section>
-      <section ref={contactRef} id="contact" style={{ padding: '50px 0' }}>
-        <Contact />
-      </section>
-    </>
+    <Grid container alignContent={'center'}>
+      <Grid item xs={12} md={12}>
+        <section id="home" ref={homeRef} style={{ padding: '50px 0' }}><h1>This is home page</h1></section>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <section ref={projectsRef} id="projects" style={{ padding: '50px 0' }}>
+          <Projects />
+        </section>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <section ref={aboutRef} id="about" style={{ padding: '50px 0' }}>
+          <About />
+        </section >
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <section ref={contactRef} id="contact" style={{ padding: '50px 0' }}>
+          <Contact />
+        </section>
+      </Grid>
+    </Grid>
   );
 };
 
