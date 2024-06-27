6d481e5a2d4dcb5ea428b8eb193fe07c459753a4
Mr. Zero <vivek248.vm@gmail.com>
2024-06-27 08:08:36 +0530

    banner added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,35 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import clouds from '../assets/images/clouds.png';
+import plane from '../assets/images/plane_1.gif';
+import TypewriterComponent from 'typewriter-effect';
+// import {Typography} from '@mui/material';
 
 const Home = () => {
   return (
+    <>
+      <div className="container">
         <img src={clouds} alt="clouds" className='banner'/>
-    // <Grid container alignItems={'center'}>
-    //   <Grid item>
-    //   </Grid>
-    // </Grid>
+        <img src={plane} alt="Overlay" className="overlay-image fly-right" />
+        <Typography
+          variant="h4"
+          component="p"
+          sx={{
+            position: 'absolute',
+            top: '40%',
+            left: '40%',
+            color: 'white',
+          }}
+         > I am
+          <TypewriterComponent
+            options={{
+              strings: ['Vivek', 'a software engineer', 'a web developer'],
+              autoStart: true,
+              loop: true
+            }}
+          />
+        </Typography>
+      </div>
+    </>
   )
 };
 
